3b9ae18d516b21ca90d00d1ed6f5eca3053af9ca
DPV <dav.parada@duocuc.cl>
2022-05-11 20:46:03 -0400

    API

--- API/src/routes/comentario.js
+++ API/src/routes/comentario.js
@@ -1,1 +1,24 @@
 const { Router } = require('express')
+const { consultar, guardar, modificar } = require('../models/comentario')
+
+const router = Router()
+
+router.get('/comentario', async (req, res) => {
+  const params = req.body
+  const respuesta = await consultar(params)
+  res.json(respuesta)
+})
+
+router.post('/comentario', async (req, res) => {
+  const params = req.body
+  const respuesta = await guardar(params)
+  res.json(respuesta)
+})
+
+router.put('/comentario', async (req, res) => {
+  const params = req.body
+  const respuesta = await modificar(params)
+  res.json(respuesta)
+})
+
+module.exports = router
--- API/src/routes/seguidor.js
+++ API/src/routes/seguidor.js
@@ -1,1 +1,24 @@
 const { Router } = require('express')
+const { consultar, guardar, modificar } = require('../models/seguidor')
+
+const router = Router()
+
+router.get('/seguidor', async (req, res) => {
+  const params = req.body
+  const respuesta = await consultar(params)
+  res.json(respuesta)
+})
+
+router.post('/seguidor', async (req, res) => {
+  const params = req.body
+  const respuesta = await guardar(params)
+  res.json(respuesta)
+})
+
+router.put('/seguidor', async (req, res) => {
+  const params = req.body
+  const respuesta = await modificar(params)
+  res.json(respuesta)
+})
+
+module.exports = router
