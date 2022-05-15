f6777d96d468fe4922e6a78f9f048c3eede8f68c
DPV <dav.parada@duocuc.cl>
2022-05-15 16:36:49 -0400

    Like, publicacion

--- API/src/routes/like.js
+++ API/src/routes/like.js
@@ -1,5 +1,5 @@
 const { Router } = require('express')
-const { consultar, guardar, modificar } = require('../models/like')
+const { consultar, meGusta, noMeGusta } = require('../models/like')
 
 const router = Router()
 
@@ -11,13 +11,13 @@ router.get('/like', async (req, res) => {
 
 router.post('/like', async (req, res) => {
   const params = req.body
-  const respuesta = await guardar(params)
+  const respuesta = await meGusta(params)
   res.json(respuesta)
 })
 
 router.put('/like', async (req, res) => {
   const params = req.body
-  const respuesta = await modificar(params)
+  const respuesta = await noMeGusta(params)
   res.json(respuesta)
 })
 
