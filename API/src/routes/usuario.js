0a06f88787de9c54c5f45462c7fdd7cf7be1e495
el-deivid2 <89952892+el-deivid2@users.noreply.github.com>
2022-05-18 19:12:55 -0400

    Update usuario.js

--- API/src/routes/usuario.js
+++ API/src/routes/usuario.js
@@ -1,11 +1,6 @@
-<<<<<<< HEAD
 /* eslint-disable linebreak-style */
 const { Router } = require('express')
 const { consultar, guardar, modificar, login } = require('../models/usuario')
-=======
-const { Router } = require('express')
-const { consultar, guardar, modificar } = require('../models/usuario')
->>>>>>> cf14752f2bf7fb106c58032602d31c06e0f70f61
 
 const router = Router()
 
@@ -21,15 +16,12 @@ router.post('/usuario', async (req, res) => {
   res.json(respuesta)
 })
 
-<<<<<<< HEAD
-router.get('/login', async (req, res) => {
+router.post('/login', async (req, res) => {
   const params = req.body
   const respuesta = await login(params)
   res.json(respuesta)
 })
 
-=======
->>>>>>> cf14752f2bf7fb106c58032602d31c06e0f70f61
 router.put('/usuario', async (req, res) => {
   const params = req.body
   const respuesta = await modificar(params)
