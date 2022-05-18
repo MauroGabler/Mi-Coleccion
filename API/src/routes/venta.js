1f865fc1ed173706360b0277d61a1369815ca6a5
el-deivid2 <89952892+el-deivid2@users.noreply.github.com>
2022-05-18 19:13:31 -0400

    Update venta.js

--- API/src/routes/venta.js
+++ API/src/routes/venta.js
@@ -1,7 +1,5 @@
-<<<<<<< HEAD
 /* eslint-disable linebreak-style */
-=======
->>>>>>> cf14752f2bf7fb106c58032602d31c06e0f70f61
+
 const { Router } = require('express')
 const { consultar, guardar, modificar } = require('../models/venta')
 
