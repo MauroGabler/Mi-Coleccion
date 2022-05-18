68d3d7b285ef36f80deec5614923b84da245c819
el-deivid2 <89952892+el-deivid2@users.noreply.github.com>
2022-05-18 19:07:09 -0400

    Update index.js

--- API/src/index.js
+++ API/src/index.js
@@ -1,7 +1,5 @@
-<<<<<<< HEAD
 /* eslint-disable linebreak-style */
-=======
->>>>>>> cf14752f2bf7fb106c58032602d31c06e0f70f61
+
 const express = require('express')
 const cors = require('cors')
 
@@ -15,18 +13,10 @@ app.use('/API', require('./routes/categoria.js'))
 app.use('/API', require('./routes/comentario.js'))
 app.use('/API', require('./routes/like.js'))
 app.use('/API', require('./routes/publicacion.js'))
-<<<<<<< HEAD
 // app.use('/API', require('./routes/reporte.js'))
 app.use('/API', require('./routes/seguidor.js'))
 app.use('/API', require('./routes/usuario.js'))
 app.use('/API', require('./routes/valoracion.js'))
 // app.use('/API', require('./routes/venta.js'))
-=======
-app.use('/API', require('./routes/reporte.js'))
-app.use('/API', require('./routes/seguidor.js'))
-app.use('/API', require('./routes/usuario.js'))
-app.use('/API', require('./routes/valoracion.js'))
-app.use('/API', require('./routes/venta.js'))
->>>>>>> cf14752f2bf7fb106c58032602d31c06e0f70f61
 
 app.listen(port, () => console.log('API REST Oracle escuchando en el puerto %s!', port))
