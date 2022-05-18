d5afe2c3b5b321c28bfbdb60f4775b49037c186a
el-deivid2 <89952892+el-deivid2@users.noreply.github.com>
2022-05-18 19:08:22 -0400

    Update articulo.js

--- API/src/routes/articulo.js
+++ API/src/routes/articulo.js
@@ -1,11 +1,6 @@
-<<<<<<< HEAD
 /* eslint-disable linebreak-style */
 const { Router } = require('express')
 const { consultar, guardar, modificar } = require('../models/articulo')
-=======
-const { Router } = require('express')
-const { consultar, guardar } = require('../models/articulo')
->>>>>>> cf14752f2bf7fb106c58032602d31c06e0f70f61
 
 const router = Router()
 
@@ -20,7 +15,6 @@ router.post('/articulo', async (req, res) => {
   const respuesta = await guardar(parametros)
   res.json(respuesta)
 })
-<<<<<<< HEAD
 
 router.put('/articulo', async (req, res) => {
   const parametros = req.body
@@ -28,7 +22,4 @@ router.put('/articulo', async (req, res) => {
   res.json(respuesta)
 })
     
-=======
-  
->>>>>>> cf14752f2bf7fb106c58032602d31c06e0f70f61
 module.exports = router
