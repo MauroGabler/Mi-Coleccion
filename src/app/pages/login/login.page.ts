97e696204af6e6a9fbe07b7731d4d1609510c2f2
DPV <dav.parada@duocuc.cl>
2022-05-26 21:39:38 -0400

    registro api

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ToastController } from '@ionic/angular';
+import { ApiService } from 'src/app/servicios/api.service';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +10,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPage implements OnInit {
 
-  constructor() { }
+  usuario: string;
+  contrasena: string;
+
+  constructor(
+    private api: ApiService,
+    private router: Router,
+    private toast: ToastController
+    ) { }
 
   ngOnInit() {
   }
 
+  login() {
+    const params = {
+      usuario: this.usuario,
+      contrasena: this.contrasena
+    };
+    console.log(params);
+
+    this.api.login(params).subscribe(msg => {
+      msg.logueado ? this.router.navigate(['home']) : this.toastMsj(msg.mensaje);
+    });
+  }
+
+  async toastMsj(mensaje) {
+    const toast = await this.toast.create({
+      message: mensaje,
+      position: 'bottom',
+      duration: 3000,
+    });
+    toast.present();
+  }
 }
--- src/app/servicios/api.service.ts
+++ src/app/servicios/api.service.ts
@@ -1,39 +1,43 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { ToastController } from '@ionic/angular';       // Libreria mensaje Toas
-import {NavigationExtras, Router,ActivatedRoute} from '@angular/router';  // IMPORTAR LIBRERIA DE RUTAS
-import { Storage } from '@capacitor/storage';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
 
-  apiBase = "http://localhost:8080/api/";
+  apiBase = 'http://localhost:8080/api/';
+  api = 'http://localhost:3000/API/';
 
 
-  constructor(private http:HttpClient,private router:Router,
-              private activateRoute: ActivatedRoute, public toastController: ToastController)
-               {
-
-
-               } // Cierre constructor
+  constructor(private http: HttpClient) { }
 
 
   httpOptions = {
-  headers: new HttpHeaders({
-    'Content-Type': 'application/json',
-    'Access-Control-Allow-Origin':'*'
-  })
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json',
+      'Access-Control-Allow-Origin': '*'
+    })
+  }
+
+
+  // Funcion permite obtener listado Categorias
+  getColecciones(): Observable<any> {
+    console.log('Api url  = ' + this.apiBase);
+    return this.http.get(this.apiBase + 'colecciones').pipe();
+  } // Cierre obtener Categorias
+
+  consultarUsuario(data): Observable<any> {
+    return this.http.get(`${this.api}usuario`, data).pipe();
+  }
+
+  registrarUsuario(data): Observable<any> {
+    return this.http.post(`${this.api}usuario`, data).pipe();
+  }
+
+  login(data): Observable<any> {
+    return this.http.post(`${this.api}login`, data).pipe();
+  }
+
 }
-
-
-// Funcion permite obtener listado Categorias
-getColecciones():Observable<any>{
-  console.log("Api url  = " + this.apiBase);
-  return this.http.get(this.apiBase + "colecciones").pipe();
-} // Cierre obtener Categorias
-
-
-}//Final
